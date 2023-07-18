74221f6655a06d22087d37c170036853eb09c61b
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2023-07-19 00:52:03 +0900

    [add] error fix

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,11 +1,13 @@
 import { Configuration, TransactionRoutesApi } from "symbol-rest";
 import symbolSdk from "symbol-sdk";
+import type { PrivateKey, Signature } from "symbol-sdk/src/CryptoTypes.js";
+import type { Transaction } from "symbol-sdk/src/symbol/models.js";
 
 const config = new Configuration();
 const transactionHttpRepo = new TransactionRoutesApi(config);
 
 const facade = new symbolSdk.facade.SymbolFacade("testnet");
-const transaction = facade.transactionFactory.create({
+const transaction: Transaction = facade.transactionFactory.create({
   type: "transfer_transaction_v1",
   signerPublicKey:
     "87DA603E7BE5656C45692D5FC7F6D0EF8F24BB7A5C10ED5FDA8C5CFBC49FCBC8",
@@ -15,25 +17,33 @@ const transaction = facade.transactionFactory.create({
   mosaics: [{ mosaicId: 0x7cdf3b117a3c40ccn, amount: 1000000n }],
 });
 
-const privateKey = new symbolSdk.PrivateKey(
+const privateKey: PrivateKey = new symbolSdk.PrivateKey(
   "EDB671EB741BD676969D8A035271D1EE5E75DF33278083D877F23615EB839FEC"
 );
 
 // type error
-const signature = facade.signTransaction(
-  new facade.constructor.KeyPair(privateKey),
+const signature: Signature = facade.signTransaction(
+  // new facade.constructor.KeyPair(privateKey), <---- type error
+  new symbolSdk.symbol.KeyPair(privateKey), // <---- type ok
   transaction
 );
 
 // type error
-const jsonPayload = facade.transactionFactory.constructor.attachSignature(
-  transaction,
-  signature
-);
+// const jsonPayload = facade.transactionFactory.constructor.attachSignature(
+//   transaction,
+//   signature
+// );
+
+// type ok
+const jsonPayload: string =
+  symbolSdk.symbol.SymbolTransactionFactory.default.attachSignature(
+    transaction,
+    signature
+  );
 
 transactionHttpRepo
   .announceTransaction({
-    transactionPayload: jsonPayload,
+    transactionPayload: JSON.parse(jsonPayload),
   })
   .then((e) => {
     console.log(e);
