c084a7ff157b69a03953b78a6abad30464c6e838
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2023-07-20 07:11:27 +0900

    [add] simplify

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,4 +1,5 @@
 import symbolSdk from "symbol-sdk";
+import type { PrivateKey, Signature } from "symbol-sdk/src/CryptoTypes.js";
 
 const facade = new symbolSdk.facade.SymbolFacade("testnet");
 const transaction = facade.transactionFactory.create({
@@ -11,23 +12,22 @@ const transaction = facade.transactionFactory.create({
   mosaics: [{ mosaicId: 0x7cdf3b117a3c40ccn, amount: 1000000n }],
 });
 
-type PrivateKey = InstanceType<typeof symbolSdk.PrivateKey>;
 const privateKey: PrivateKey = new symbolSdk.PrivateKey(
   "EDB671EB741BD676969D8A035271D1EE5E75DF33278083D877F23615EB839FEC"
 );
 
-type Signature = InstanceType<typeof symbolSdk.Signature>;
 const signature: Signature = facade.signTransaction(
   new symbolSdk.symbol.KeyPair(privateKey),
   transaction
 );
-
-console.log(privateKey.toString());
-console.log(signature.toString());
 
 const jsonPayload: string = symbolSdk.symbol.TransactionFactory.attachSignature(
   transaction,
   signature
 );
 
-console.log(jsonPayload);
+console.log({
+  privateKey: privateKey.toString(),
+  signature: signature.toString(),
+  jsonPayload,
+});
